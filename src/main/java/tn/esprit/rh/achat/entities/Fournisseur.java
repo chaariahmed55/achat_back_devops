2508708b6210f85834b9962f5f41cf5924bbdec5
chaariahmed55 <79192311+chaariahmed55@users.noreply.github.com>
2022-11-09 00:10:12 +0100

    add fournisseur class

--- src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
+++ src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
@@ -44,6 +44,14 @@ public class Fournisseur implements Serializable {
     @OneToOne(cascade= CascadeType.ALL,fetch=FetchType.EAGER)
     private DetailFournisseur detailFournisseur;
     
+    
+    public Fournisseur(long idFournisseur, String code, String libelle, CategorieFournisseur ordinaire) {
+        this.idFournisseur = idFournisseur;
+        this.code = code;
+        this.libelle = libelle;
+        this.categorieFournisseur = ordinaire;
+    }
 
+    
 	
 }
